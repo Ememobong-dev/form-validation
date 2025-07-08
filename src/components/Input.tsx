cbad750ff4c9f2fd2a3afabc9298eca64c404dfc
Olushola <ememobongumoren2@gmail.com>
2025-07-08 17:31:29 +0100

    update on input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import { IoMdEyeOff } from "react-icons/io";
+"use client";
+
+import React, { useState } from 'react'
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 export const Input = ({ inputType, inputlabel, inputImp }: { inputType: string; inputlabel: string; inputImp?: boolean }) => {
+    const [openEye, setOpenEye] = useState(false);
+
     return (
         <>
             <p>
                 {inputlabel}
                 {inputImp && <span className="text-red-700">*</span>}
             </p>
-            <div className="flex w-full py-2 px-2 border text-sm">
-                <input type={inputType} className="focus:outline-0 " />
+            <div className="flex items-center w-full py-2 px-2 border text-sm">
+                <input type={inputType === "password" && openEye ? "text" : inputType} className="focus:outline-0 " />
                 {inputType === 'password' && (
                     <>
-                        <IoMdEyeOff />
+                        {openEye ? (
+                            <IoMdEye className='cursor-pointer' onClick={() => setOpenEye(!openEye)} />
+
+                        ) : (
+                            <IoMdEyeOff className='cursor-pointer' onClick={() => setOpenEye(!openEye)} />
+                        )
+
+                        }
+
 
                     </>
                 )
